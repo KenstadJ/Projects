d534aba7082d6358964e8b2eda26df99ceb829e0
KenstadJ <75332800+KenstadJ@users.noreply.github.com>
2022-06-23 13:52:40 -0500

    Add files via upload

---
+++ server.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>Thank You</title></head>
+
+<body>
+    <?php
+        $name = $_POST["service"];
+        echo "You rated our service:" . $name ."/10";
+
+    ?>
+
+</body>
+</html>
